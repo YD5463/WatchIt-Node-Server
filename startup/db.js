cb87a419938f8f08a323972dbc3c604358b561b1
yosef555 <yosefdanan@gmail.com>
2020-04-27 08:54:51 +0300

    fix db connection bug

--- startup/db.js
+++ startup/db.js
@@ -1,9 +1,11 @@
-const winston = require('winston');
-const mongoose = require('mongoose');
-const config = require('config');
+const winston = require("winston");
+const mongoose = require("mongoose");
+const config = require("config");
 
-module.exports = function() {
-  const db = config.get('db');
-  mongoose.connect(db)
-    .then(() => winston.info(`Connected to ${db}...`));
-}
+module.exports = function () {
+  const db = config.get("db");
+  mongoose
+    .connect(db)
+    .then(() => winston.info(`Connected to ${db}...`))
+    .catch((reason) => console.log("field to connect mongo db"));
+};
